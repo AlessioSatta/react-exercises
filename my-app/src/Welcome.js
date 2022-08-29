0cd439ca64bbe897a4666e27594febeedf32b407
Alessio Satta <alessio@MBP-Alessio.local>
2022-08-29 17:15:45 +0200

    add: jsx expression name in welcome.js

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+const name = 'Alessio'
+
 export class Welcome extends React.Component {
     render() {
         return(
             <div>
-                <p>Welcome, {this.props.name}!</p>
+                <p>Welcome, <strong>{name}</strong>!</p>
                 <p>Your age is {this.props.age}</p>
             </div>
         )
