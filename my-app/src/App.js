03b039716b7471fd891fd5bcc9064aec4abdec92
Alessio Satta <alessio@MBP-Alessio.homenet.telecomitalia.it>
2022-09-07 11:38:32 +0200

    add: condition in welcome.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return(
             <div>
                 <Hello />
-                <Welcome age= {34} />
+                <Welcome age= {17} />
             </div>
         ) 
     }
