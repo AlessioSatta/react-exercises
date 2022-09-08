e1472c0867ca5d8c4c321a63eb3b667bb0f538a3
Alessio Satta <alessio@MBP-Alessio.local>
2022-09-08 10:57:21 +0200

    add: passing value as props from app.js to counter.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ export class App extends React.Component {
             <div>
                 <Hello />
                 <Welcome age= {17} />
-                <Counter />
+                <Counter initialValue= {0} incrementAmount= {1} incrementInterval= {1000} />
             </div>
         ) 
     }
