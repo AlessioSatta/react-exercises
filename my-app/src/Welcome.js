118d7191c09ae32049db6b1e4e0c1aafd65ed0d3
Alessio Satta <alessio@MBP-Alessio.local>
2022-09-08 10:26:46 +0200

    john condition in welcome.js

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Age } from "./Age";
 
-const name = 'Alessio'
+// const name = 'Alessio'
 
 export class Welcome extends React.Component {
     render() {
         return(
             <div>
-                <p>Welcome, <strong>{name}</strong>!</p>
-                {this.props.age > 18 && this.props.age < 65 && <Age age= {this.props.age} />}
+                <p>Welcome, <strong>{this.props.name}</strong>!</p>
+                {this.props.age > 18 && this.props.age < 65 && this.props.name === 'John' && <Age age= {this.props.age} />}
             </div>
         )
     }
 }
 Welcome.defaultProps = {
-    name: 'Marco Aurelio'
+    name: 'John'
 }
