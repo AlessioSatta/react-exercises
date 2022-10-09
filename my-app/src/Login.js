4dead1f965aaba44ca6bc9628805f3617c4d3b5b
Alessio Satta <alessio@MBP-Alessio.fritz.box>
2022-10-09 16:17:02 +0200

    add: button in Login component

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -18,12 +18,17 @@ export class Login extends React.Component {
         })
     }
 
+    onLogin = () => {
+        
+    }
+
     render() {
         return (
             <div>
-                <input name="username" onChange={this.handleInputChange}></input>
-                <input name="password" type="password" onChange={this.handleInputChange}></input>
+                <input name="username" value={this.state.value} onChange={this.handleInputChange}></input>
+                <input name="password" value={this.state.value} type="password" onChange={this.handleInputChange}></input>
                 <input name="remember" type="checkbox" onChange={this.handleInputChange}></input>
+                <button disabled={!this.state.username || !this.state.password} onClick={this.onLogin}>login</button>
             </div>
         )
     }
