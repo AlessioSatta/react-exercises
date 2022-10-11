02a0c8e79b425cb516e6fbf7b53cae43477fb0eb
Alessio Satta <alessio@MBP-Alessio.fritz.box>
2022-10-11 07:37:02 +0200

    fix: some fix in Login component

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -18,16 +18,18 @@ export class Login extends React.Component {
         })
     }
 
-    onLogin = () => {
-        
+    onLogin = (event) => {
+        event.preventDefault()
+
+        console.log(this.state)
     }
 
     render() {
         return (
             <div>
-                <input name="username" value={this.state.value} onChange={this.handleInputChange}></input>
-                <input name="password" value={this.state.value} type="password" onChange={this.handleInputChange}></input>
-                <input name="remember" type="checkbox" onChange={this.handleInputChange}></input>
+                <input name="username" value={this.state.username} onChange={this.handleInputChange}></input>
+                <input name="password" value={this.state.password} type="password" onChange={this.handleInputChange}></input>
+                <input name="remember" value={this.state.remember} type="checkbox" onChange={this.handleInputChange}></input>
                 <button disabled={!this.state.username || !this.state.password} onClick={this.onLogin}>login</button>
             </div>
         )
