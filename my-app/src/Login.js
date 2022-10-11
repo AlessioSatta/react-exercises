cdaa45563fb87cd7476e10f9232a0201752acff0
Alessio Satta <alessio@MBP-Alessio.fritz.box>
2022-10-11 07:39:58 +0200

    fix: some fix in Login component

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -23,7 +23,7 @@ export class Login extends React.Component {
             <div>
                 <input name="username" value={this.state.username} onChange={this.handleInputChange}></input>
                 <input name="password" value={this.state.password} type="password" onChange={this.handleInputChange}></input>
-                <input name="remember" type="checkbox" onChange={this.handleInputChange}></input>
+                <input name="remember" value={this.state.remember} type="checkbox" onChange={this.handleInputChange}></input>
             </div>
         )
     }
