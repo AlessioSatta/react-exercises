5b348f9c871de738478af9a6a4c027f01fda1397
Alessio Satta <alessio@MBP-Alessio.fritz.box>
2022-10-20 18:53:50 +0200

    add: functional component in ClickCounter component

--- my-app/src/ClickCounter.js
+++ my-app/src/ClickCounter.js
@@ -1,24 +1,41 @@
-import React from 'react'
+// import React from 'react'
 
-export class ClickCounter extends React.Component {
-    state = {
-        count: 0
+// export class ClickCounter extends React.Component {
+//     state = {
+//         count: 0
+//     }
+
+//     handleCounterIncrement = () => {
+//         this.setState((state) => {
+//             return {
+//                 count: state.count + 1
+//             }
+//         })
+//     }
+
+//     render() {
+//         return(
+//             <div>
+//                 <h3>Count: {this.state.count}</h3>
+//                 <button onClick={this.handleCounterIncrement}>Increment</button>
+//             </div>
+//         )
+//     }
+// }
+
+import { useState } from 'react';
+
+export function ClickCounter() {
+    const [counter, setCounter] = useState(0)
+
+    function handleCounterIncrement() {
+        setCounter(c => c + 1)
     }
 
-    handleCounterIncrement = () => {
-        this.setState((state) => {
-            return {
-                count: state.count + 1
-            }
-        })
-    }
-
-    render() {
-        return(
-            <div>
-                <h3>Count: {this.state.count}</h3>
-                <button onClick={this.handleCounterIncrement}>Increment</button>
-            </div>
-        )
-    }
+    return(
+        <div>
+            <h3>Counter: {counter}</h3>
+            <button onClick={handleCounterIncrement}>Increment</button>
+        </div>
+    )
 }
