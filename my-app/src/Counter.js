1f4d0ffd5e6efad7ba8bd7c477a0d2013868a635
Alessio Satta <alessio@MBP-Alessio.fritz.box>
2022-10-21 15:05:13 +0200

    add: useEffect in Counter component

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
-export class Counter extends React.Component {
-    state = {
-        count: this.props.initialValue,
-    }
+// export class Counter extends React.Component {
+//     state = {
+//         count: this.props.initialValue,
+//     }
     
-    componentDidMount() {
-        setInterval(() => {
-            this.setState({
-                count: this.state.count + this.props.incrementAmount,
-            })
-        }, this.props.incrementInterval)
-    }
+//     componentDidMount() {
+//         setInterval(() => {
+//             this.setState({
+//                 count: this.state.count + this.props.incrementAmount,
+//             })
+//         }, this.props.incrementInterval)
+//     }
     
-    render() {
-        return <CounterDisplay count={this.state.count} />
-    }
+//     render() {
+//         return <CounterDisplay count={this.state.count} />
+//     }
+// }
+
+export function Counter(props) {
+    const [count, setCount] = useState(props.initialValue)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount(c => c + props.incrementAmount)
+        }, props.incrementInterval)
+        return () => {
+            clearInterval(interval)
+        }
+    })
+
+    return (
+        <CounterDisplay count={count} />
+    )
 }
