212e74930e0e459393566c231b2b2bd8c9a71810
Alessio Satta <alessio@MBP-Alessio.fritz.box>
2022-10-31 10:55:27 +0100

    feat: useContext in DisplayLanguage component

--- my-app/src/DisplayLanguage.js
+++ my-app/src/DisplayLanguage.js
@@ -1,11 +1,24 @@
-import React from "react";
+// import React from "react";
 
-export class DisplayLanguage extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.language === 'English' ? <h1>Current language is: {this.props.language}</h1> : <h1>La lingua corrente è: {this.props.language}</h1>}
-            </div>
-        )
-    }
+// export class DisplayLanguage extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 {this.props.language === 'English' ? <h1>Current language is: {this.props.language}</h1> : <h1>La lingua corrente è: {this.props.language}</h1>}
+//             </div>
+//         )
+//     }
+// }
+
+import { useContext } from 'react';
+import { LanguageContext } from './LanguageContext';
+
+export function DisplayLanguage() {
+    const language = useContext(LanguageContext)
+
+    return (
+        <div>
+            {language === 'English' ? <h1>Current language is: {language}</h1> : <h1>La lingua corrente è: {language}</h1>}
+        </div>
+    )
 }
