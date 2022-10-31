8760f49dd6602d78a1da74de747c16374f7de11b
Alessio Satta <alessio@MBP-Alessio.fritz.box>
2022-10-31 17:33:07 +0100

    add: counter path in App component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -62,11 +62,13 @@
 
 import { Route, Routes } from 'react-router-dom';
 import { Welcome } from './Welcome'
+import { Counter } from './Counter'
 
 export function App() {
     return (
         <Routes>
             <Route path='/' element={<Welcome name='Alessio' />} />
+            <Route path='/counter' element={<Counter initialValue={0} incrementAmount={1} incrementInterval={1000} /> } />
         </Routes>
     )
 }
